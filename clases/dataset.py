c73cd1e91f97fa73dfca98f79ca246ac8d7a5239
marzvk <marianozovak1@gmail.com>
2025-06-03 12:40:03 -0300

    primer commit entrega-1

---
+++ clases/dataset.py
@@ -0,0 +1,23 @@
+"Clase Padre"
+from abc import ABC, abstractmethod
+import pandas as pd
+
+
+class Dataset(ABC):
+    """clase principal abstarcta, una clase que no puede ser instanciada, es obligatorio que una subclase la herede para poder instanciar""" 
+
+    def __init__(self, fuente):
+        self.__fuente = fuente
+        self.__datos = None
+
+    @property
+    def datos(self):
+        """metodo publico para devolver atributo privado"""
+        return self.__datos
+    
+    @datos.setter
+    def datos(self, valor):
+        """metodo para modificar datos(setter) con el valor pasado,si pasa las validaciones dadas"""
+        if not isinstance(valor, pd.DataFrame):
+            raise ValueError("El elemento no es un df de pandas")
+        self.__datos = valor
